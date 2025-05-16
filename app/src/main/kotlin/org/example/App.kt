899452fe451c4877b6913c184f271c8c3a7e0184
Noah Graña <noahlucianograna@gmail.com>
2025-05-16 10:02:38 -0300

    Etapa 1: Implementación de funciones básicas

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -54,13 +54,13 @@ fun main() {
 
 // Etapa 1
 fun calcularPromedio(nota1: Double, nota2: Double): Double {
-    // Implementar aquí
-    return 0.0
+    return (nota1+nota2)/2
 }
 
 fun esAprobado(nota: Double): Boolean {
-    // Implementar aquí
-    return false
+  if (nota>=6){
+    return true
+  } else {return false}
 }
 
 // Etapa 2
