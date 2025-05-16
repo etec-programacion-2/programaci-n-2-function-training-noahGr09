f9c3f87ab0ee89e322c83c8b0838befba23ee630
Noah Graña <noahlucianograna@gmail.com>
2025-05-16 10:07:40 -0300

    Etapa 2: Implementación de funciones con múltiples parámetros

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -66,12 +66,16 @@ fun esAprobado(nota: Double): Boolean {
 // Etapa 2
 fun calcularPromedioTresNotas(nota1: Double, nota2: Double, nota3: Double): Double {
     // Implementar aquí
-    return 0.0
+    return (nota1+nota2+nota3)/3
 }
 
 fun obtenerEstadoAlumno(nombre: String, apellido: String, nota: Double): String {
     // Implementar aquí
-    return ""
+    if (nota >= 7.0){
+        return "El alumno $nombre $apellido está aprobado"
+    } else{
+        return "El alumno $nombre $apellido está desaprobado"
+    }
 }
 
 // Etapa 3
